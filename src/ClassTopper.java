b62bf181d4d3162a678cab85cdc773a3259b42b8
VarshaaSK <varshaa.sk@m2pfintech.com>
2023-03-25 13:53:11 +0530

    Add. Logic Added to find if the Sum of Odd digits and Even Digits are same

--- src/ClassTopper.java
+++ src/ClassTopper.java
@@ -1,5 +1,37 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class ClassTopper {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Registration Number : ");
+
+        String registrationNumber = scan.nextLine();
+
+        int sumOfOddDigits = 0;
+        int sumOfEvenDigits = 0;
+
+
+        char[] numberArray = registrationNumber.toCharArray();
+
+        for(int iterator = 0 ; iterator < numberArray.length ; iterator++){
+            if(numberArray[iterator] % 2 == 0){
+                sumOfEvenDigits = sumOfEvenDigits + Character.getNumericValue(numberArray[iterator]);
+            }
+            else{
+                sumOfOddDigits = sumOfOddDigits + Character.getNumericValue(numberArray[iterator]);
+            }
+        }
+
+        if(sumOfEvenDigits == sumOfOddDigits){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no");
+        }
+
+        }
+
 }
